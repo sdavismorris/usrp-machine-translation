5eda9bb10a18251455118913e0132efe8c447966
York Ng <yorkhayng@gmail.com>
2026-03-08 16:55:48 -0400

    final evaluation script

--- evaluation.py
+++ evaluation.py
@@ -1,22 +1,22 @@
-import os
 import pandas as pd
 import evaluate
 import torch
-from bert_score import score as bert_score
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
 
 def evaluate_bertscore(references: list, hypotheses: list) -> list[float]:
-    P, R, F1 = bert_score(
-        hypotheses,
-        references,
-        model_type="microsoft/deberta-xlarge-mnli",
-        device="cuda",
-        batch_size=32
+    metric = evaluate.load("bertscore")
+    results = metric.compute(
+        predictions=hypotheses,
+        references=references,
+        model_type="xlm-roberta-large",
+        num_layers=12,
+        batch_size=16,
+        device=device
     )
-    return F1.tolist()
+    return results["f1"]
 
 
 def evaluate_bleurt(references: list, hypotheses: list) -> list[float]:
@@ -24,19 +24,16 @@ def evaluate_bleurt(references: list, hypotheses: list) -> list[float]:
     results = metric.compute(
         predictions=hypotheses,
         references=references,
-        batch_size=16
     )
     return results["scores"]
 
 
-def evaluate_comet(references: list, hypotheses: list) -> list[float]:
+def evaluate_comet(sources: list, references: list, hypotheses: list) -> list[float]:
     metric = evaluate.load("comet")
     results = metric.compute(
+        sources=sources,
         predictions=hypotheses,
         references=references,
-        model_name_or_path="Unbabel/wmt22-comet-da",
-        batch_size=8,
-        gpus=1
     )
     return results["scores"]
 
@@ -44,10 +41,11 @@ def evaluate_comet(references: list, hypotheses: list) -> list[float]:
 df = pd.read_csv("metric_data.csv")
 references = df["reference"].tolist()
 hypotheses = df["translation"].tolist()
+sources = df["source"].tolist()
 
 bertscore_eval = evaluate_bertscore(references, hypotheses)
 bleurt_eval = evaluate_bleurt(references, hypotheses)
-comet_eval = evaluate_comet(references, hypotheses)
+comet_eval = evaluate_comet(sources, references, hypotheses)
 
 df["bertscore"] = bertscore_eval
 df["bleurt"] = bleurt_eval
